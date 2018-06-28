c9277b17c39666c105cfe5b7b6840ad0d191319d
Benjamin Such <benjamin.such@gmail.com>
2018-06-28 22:59:59 +0200

    Fixed the issue where a preset value for the ng-model did not validate correctly when initially loaded.

--- src/ng-checkbox.js
+++ src/ng-checkbox.js
@@ -15,43 +15,41 @@
         ngCheckboxUnchecked: '=',
         disabled: '='
       },
-      template: '<button class="ng-checkbox" type="button" ng-click="toggle();" ng-disabled="disabled">' +
+      template: '<button class="ng-checkbox" ng-class="{checked: currentVal === checkedVal, unchecked: currentVal !== checkedVal}" type="button" ng-click="toggle();" ng-disabled="disabled">' +
                 '  <span class="{{currentVal === checkedVal ? options.checkedClass : options.uncheckedClass}}"></span>' +
                 '</button>',
       link: (scope, element, attrs, ngModelController) => {
+        const defaultOptions = {
+          checkedClass: 'glyphicon glyphicon-ok',
+          uncheckedClass: 'glyphicon'
+        };
+    
+        scope.options = {};
+    
+        scope.toggle = () => {
+          scope.currentVal = scope.currentVal === scope.checkedVal ? scope.uncheckedVal : scope.checkedVal;
+        };
+    
+        scope.$watch('ngCheckboxChecked', val => {
+          scope.checkedVal = val ? val : true;
+          ngModelController.$validate();
+        });
+    
+        scope.$watch('ngCheckboxUnchecked', val => {
+          scope.uncheckedVal = val ? val : false;
+          ngModelController.$validate();
+        });
+    
+        scope.$watchCollection('ngCheckboxOptions', (val = {}) => {
+          angular.extend(scope.options, defaultOptions, val);
+        });
+
         ngModelController.$validators.invalid = (modelValue, viewValue) => {
           let val = modelValue || viewValue;
           return val === scope.checkedVal || val === scope.uncheckedVal;
         };
-      },
-      controller: ngCheckboxController
+      }
     };
   }]);
-
-  ngCheckboxController.$inject = ['$scope'];
-  function ngCheckboxController($scope) {
-    const defaultOptions = {
-      checkedClass: 'glyphicon glyphicon-ok',
-      uncheckedClass: 'glyphicon'
-    };
-
-    $scope.options = {};
-
-    $scope.toggle = () => {
-      $scope.currentVal = $scope.currentVal === $scope.checkedVal ? $scope.uncheckedVal : $scope.checkedVal;
-    };
-
-    $scope.$watch('ngCheckboxChecked', val => {
-      $scope.checkedVal = val ? val : true;
-    });
-
-    $scope.$watch('ngCheckboxUnchecked', val => {
-      $scope.uncheckedVal = val ? val : false;
-    });
-
-    $scope.$watchCollection('ngCheckboxOptions', (val = {}) => {
-      angular.extend($scope.options, defaultOptions, val);
-    });
-  }
 
 })();
